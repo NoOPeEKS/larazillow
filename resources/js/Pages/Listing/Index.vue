bfe698438e8c6a3d57001743be7c8a53e20f559b
NoOPeEKS <arnauapps@gmail.com>
2023-12-03 00:14:11 +0100

    Created resource controller for Listings and Vue pages for listings

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`${listing.id}`">{{listing.street}} {{ listing.street_nr }}, 
+            {{ listing.city }}, for {{ listing.price }}€</Link>
+    </div>
+</template>
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        listings: Array,
+    });
+</script>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>{{ listings.street }} {{ listings.street_nr }}, 
+    {{ listings.city }}, for {{ listings.price }}€</div>
+</template>
+<script setup>
+    defineProps({
+        listings: Object,
+    });
+</script>
