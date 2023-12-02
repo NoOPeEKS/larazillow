9aae65c42b4399c1da8a5c825f38117f9f5bd4cd
NoOPeEKS <arnauapps@gmail.com>
2023-12-02 21:33:05 +0100

    Added a default layout for the application

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -7,10 +7,3 @@
         message: String
     });
 </script>
-
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    export default {
-        layout: MainLayout
-    }
-</script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,15 +1,7 @@
 <template>
-    <MainLayout>
         <h1>Show Page</h1>
-    </MainLayout>
 </template>
 
 <script setup>
 </script>
 
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-    export default {
-        layout: MainLayout
-    }
-</script>
