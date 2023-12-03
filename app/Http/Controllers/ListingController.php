f1ea7fd71f1a07bdbc11d02128ccdf62d6fe6199
NoOPeEKS <arnauapps@gmail.com>
2023-12-03 02:48:30 +0100

    Added delete functionality to Listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -87,8 +87,10 @@ class ListingController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Listing $listing)
     {
-        //
+        $listing->delete();
+
+        return redirect()->back()->with('success', 'Listing was deleted!');
     }
 }
