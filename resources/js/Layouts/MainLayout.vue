9f4e9996cd009bf8183cff347590c88f57389089
NoOPeEKS <arnauapps@gmail.com>
2023-12-03 02:03:41 +0100

    Changed fhashSuccess when creating listing for easy code reading

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,19 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div v-if="page.props.flash.success" class="success">
-        {{ page.props.flash.success }}
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
     </div>
     <slot>Default Value</slot>
 </template>
 
 <script setup>
     import { Link, usePage } from '@inertiajs/vue3';
+    import { computed } from 'vue';
 
     const page = usePage();
+
+    const flashSuccess = computed(() => page.props.flash.success, );
 
 </script>
 
