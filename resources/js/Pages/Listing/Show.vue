7167bffe9a7de6a2b5a28fcbf703607e97c73d85
NoOPeEKS <arnauapps@gmail.com>
2023-12-05 00:51:11 +0100

    Added monthly payment calculator to Show listing and created useMonthlyPayment composable

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -14,19 +14,41 @@
             </Box>
             <Box>
                 <template #header>
-                    Offer
+                    Monthly payment
                 </template>
-                Make an offer
+                <div>
+                    <label class="label">Interest Rate ({{ interestRate }}%)</label>
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" 
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+                    <label class="label">Duration ({{ duration }} years)</label>
+                    <input v-model.number="duration" type="range" min="3" max="35" step="1" 
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly payment</div>
+                        <Price :price="monthlyPayment" class="text-3xl"/>
+                    </div>
+                </div>
             </Box>
         </div>
     </div>
 </template>
+
 <script setup>
     import ListingAddress from '@/Components/ListingAddress.vue';
     import Price from '@/Components/Price.vue';
     import ListingSpace from '@/Components/ListingSpace.vue';
     import Box from '@/Components/UI/Box.vue';
-    defineProps({
+    import { ref } from 'vue';
+    import { useMonthlyPayment } from '@/Composables/useMonthlyPayment';
+    const interestRate = ref(2.5);
+    const duration = ref(25);
+
+    const props = defineProps({
         listings: Object,
     });
+
+    const { monthlyPayment } = useMonthlyPayment(props.listings.price, interestRate, duration)
+
 </script>
