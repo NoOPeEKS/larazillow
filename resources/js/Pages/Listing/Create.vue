0e409f5d455aacfc4b34971d63f458c83d448d97
NoOPeEKS <arnauapps@gmail.com>
2023-12-04 22:41:06 +0100

    Used tailwind directives to create general classes for label, input and btn-primary components

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -2,85 +2,75 @@
 <form @submit.prevent="create">
   <div class="grid grid-cols-6 gap-4">
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Beds</label>
-      <input v-model.number="form.beds" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Beds</label>
+      <input v-model.number="form.beds" type="text" class="input"/>
       <div v-if="form.errors.beds">
         {{ form.errors.beds }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Baths</label>
-      <input v-model.number="form.baths" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" />
+      <label class="label">Baths</label>
+      <input v-model.number="form.baths" type="text" class="input" />
       <div v-if="form.errors.baths">
         {{ form.errors.baths }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Area</label>
-      <input v-model.number="form.area" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Area</label>
+      <input v-model.number="form.area" type="text" class="input"/>
       <div v-if="form.errors.area">
         {{ form.errors.area }}
       </div>
     </div>
 
     <div class="col-span-4">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">City</label>
-      <input v-model="form.city" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">City</label>
+      <input v-model="form.city" type="text" class="input"/>
       <div v-if="form.errors.city">
         {{ form.errors.city }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Post Code</label>
-      <input v-model="form.code" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Post Code</label>
+      <input v-model="form.code" type="text" class="input"/>
       <div v-if="form.errors.code">
         {{ form.errors.code }}
       </div>
     </div>
 
     <div class="col-span-4">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Street</label>
-      <input v-model="form.street" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Street</label>
+      <input v-model="form.street" type="text" class="input"/>
       <div v-if="form.errors.street">
         {{ form.errors.street }}
       </div>
     </div>
 
     <div class="col-span-2">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Street Nr</label>
-      <input v-model.number="form.street_nr" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Street Nr</label>
+      <input v-model.number="form.street_nr" type="text" class="input"/>
       <div v-if="form.errors.street_nr">
         {{ form.errors.street_nr }}
       </div>
     </div>
 
     <div class="col-span-6">
-      <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Price</label>
-      <input v-model.number="form.price" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600"/>
+      <label class="label">Price</label>
+      <input v-model.number="form.price" type="text" class="input"/>
       <div v-if="form.errors.price">
         {{ form.errors.price }}
       </div>
     </div>
 
     <div class="col-span-6">
-      <button type="submit" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">Create</button>
+      <button type="submit" class="btn-primary">Create</button>
     </div>
   </div>
 </form>
 </template>
-  
-  <style scoped>
-  label {
-    margin-right: 2em;
-  }
-  
-  div {
-    padding: 2px
-  }
-  </style>
 
 <script setup>
     import { useForm } from '@inertiajs/vue3';
