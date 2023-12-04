6a3ed54bcc7d6e96631f2442853ffecfac813968
NoOPeEKS <arnauapps@gmail.com>
2023-12-04 22:17:42 +0100

    Modified show page to accept image slots and offer slot

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,10 +1,31 @@
 <template>
-    <div>
-        <ListingAddress :listing="listings" />
+    <div class="flex flex-col-reverse md:grid grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">No images</div>
+        </Box>
+        <div class="md:col-span-5 flex flex-col gap-4">
+            <Box>
+                <template #header>
+                    Basic info:
+                </template>
+                <Price :price="listings.price" class="text-2xl font-bold"/>
+                <ListingSpace :listing="listings" class="text-lg"/>
+                <ListingAddress :listing="listings" class="text-gray-500"/>
+            </Box>
+            <Box>
+                <template #header>
+                    Offer
+                </template>
+                Make an offer
+            </Box>
+        </div>
     </div>
 </template>
 <script setup>
     import ListingAddress from '@/Components/ListingAddress.vue';
+    import Price from '@/Components/Price.vue';
+    import ListingSpace from '@/Components/ListingSpace.vue';
+    import Box from '@/Components/UI/Box.vue';
     defineProps({
         listings: Object,
     });
