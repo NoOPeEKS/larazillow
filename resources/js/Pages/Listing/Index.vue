82a6e139993010e7543ec4703b868ea8f684af24
NoOPeEKS <arnauapps@gmail.com>
2023-12-04 21:56:08 +0100

    Created Price component for better styling Listing boxes

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,6 +3,7 @@
     <Box v-for="listing in listings" :key="listing.id">
         <div>
             <Link :href="route('listing.show', {listing: listing.id})">
+                <Price :price="listing.price" class="text-2xl font-bold"/>
                 <ListingSpace :listing="listing" class="text-lg"/>
                 <ListingAddress :listing="listing" class="text-gray-500"/>
             </Link>
@@ -21,6 +22,7 @@
     import Box from '@/Components/UI/Box.vue';
     import ListingAddress from '@/Components/ListingAddress.vue';
     import ListingSpace from '@/Components/ListingSpace.vue';
+    import Price from '@/Components/Price.vue';
     defineProps({
         listings: Array,
     });
