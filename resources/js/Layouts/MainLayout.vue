ec2694027367ab6faefe7dfb8303552c510cf092
NoOPeEKS <arnauapps@gmail.com>
2023-12-06 01:33:52 +0100

    Changed main layout view for non-authenticated users

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,8 +8,13 @@
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{user.name}}</div>
           <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+          <div>Logout</div>
+        </div>
+        <div v-else>
+          <Link :href="route('login')">Log In</Link>
         </div>
       </nav>
     </div>
@@ -29,4 +34,6 @@
 
     const page = usePage();
     const flashSuccess = computed(() => page.props.flash.success, );
+
+    const user = computed(() => page.props.user);
 </script>
