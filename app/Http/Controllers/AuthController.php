24370a9e62fe71db5881c68f2de4cb651916b8f2
NoOPeEKS <arnauapps@gmail.com>
2023-12-06 01:06:24 +0100

    Users can now log in and get a session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -11,7 +13,18 @@ class AuthController extends Controller
     }
 
     public function store(Request $request) {
-        // Create the session if data valid
+        if(!Auth::attempt($request->validate([
+            'email'=> 'required|string|email',
+            'password' => 'required|string'
+        ]), true)) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentication failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended();
     }
 
     public function destroy(Request $request) {
