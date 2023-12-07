90b8868493971e1fee3d5b26d8bbe03aef7e127e
NoOPeEKS <arnauapps@gmail.com>
2023-12-08 00:49:13 +0100

    Added pagination to listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -22,7 +22,7 @@ class ListingController extends Controller
     public function index()
     {
         return inertia("Listing/Index", [
-            'listings' => Listing::all(),
+            'listings' => Listing::orderByDesc('created_at')->paginate(10),
         ]);
  
     }
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,15 @@
 <template>
    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <Listing v-for="listing in listings" :key="listing.id" :listing="listing"/>
-   </div> 
+    <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing"/>
+   </div>
+   <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-8">
+    <Pagination :links="listings.links"/>
+   </div>
 </template>
 <script setup>
-    import { Link } from '@inertiajs/vue3';
     import Listing from '@/Pages/Listing/Index/Components/Listing.vue';
-    import ListingAddress from '@/Components/ListingAddress.vue';
-    import ListingSpace from '@/Components/ListingSpace.vue';
-    import Price from '@/Components/Price.vue';
+    import Pagination from '@/Components/UI/Pagination.vue';
     defineProps({
-        listings: Array,
+        listings: Object,
     });
 </script>
