62d5349ef3550e3478b979255a5eb59a6d56c550
NoOPeEKS <arnauapps@gmail.com>
2023-12-08 18:58:51 +0100

    Deleted checkbox on realtor listings now displays deleted listings too

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -12,10 +12,18 @@ class RealtorListingController extends Controller
         $this->authorizeResource(Listing::class, 'listing');
     }
 
-    public function index() {
+    public function index(Request $request) {
+        $filters = [
+            'deleted' => $request->boolean('deleted'),
+        ];
         return inertia(
             "Realtor/Index", 
-            ['listings' => Auth::user()->listings]
+            ['listings' => Auth::user()
+                            ->listings()
+                            ->mostRecent()
+                            ->filter($filters)
+                            ->get()
+            ]
         );
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -42,6 +42,9 @@ class Listing extends Model
         )->when(
             $filters['areaTo'] ?? false,
             fn ($query, $value) => $query->where('area', '<=', $value)
+        )->when(
+            $filters['deleted'] ?? false,
+            fn ($query, $value) => $query->withTrashed()
         );
     }
 }
