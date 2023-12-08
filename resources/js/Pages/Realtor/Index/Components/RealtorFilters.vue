b25d49b5d5711c8e8c091aadb989bf4a9750666c
NoOPeEKS <arnauapps@gmail.com>
2023-12-08 18:45:59 +0100

    Added lodash dependency to wait before applying checkbox filters in realtor page

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -12,16 +12,16 @@
 <script setup>
 import { reactive, watch } from 'vue';
 import { router } from '@inertiajs/vue3';
-
+import { debounce } from 'lodash';
 const filterForm = reactive({
     deleted: false,
 })
 
 watch(
-    filterForm, () => router.get(
+    filterForm, debounce(() => router.get(
         route('realtor.listing.index'),
         filterForm,
-        {preserveState: true, preserveScroll: true}
-    )
+        { preserveState: true, preserveScroll: true },
+    ), 1000),
 );
 </script>
