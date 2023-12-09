5170d63fe51625977c5f34ab156922da5af7b463
NoOPeEKS <arnauapps@gmail.com>
2023-12-09 19:35:24 +0100

    Number of offers can now be seen in owner listing panel

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -25,9 +25,16 @@ class RealtorListingController extends Controller
                             ->listings()
                             ->filter($filters)
                             ->withCount('images')
+                            ->withCount('offers')
                             ->paginate(10)
                             ->withQueryString()
             ]
+        );
+    }
+
+    public function show(Listing $listing) {
+        return inertia("Realtor/Show",
+                    ['listing' => $listing->load('offers')]
         );
     }
 
