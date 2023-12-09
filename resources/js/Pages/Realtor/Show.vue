0fee4b2d5587f6b0be79a4c22c505ca8036776c4
NoOPeEKS <arnauapps@gmail.com>
2023-12-09 21:58:32 +0100

    Scaffolded showing every offer along with basic listing info

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,3 +1,36 @@
 <template>
-    Hello
+    <div class="mb-4">
+        <Link :href="route('realtor.listing.index')">Go back to Listings</Link>
+    </div>
+    <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+            <div class="w-full text-center font-medium text-gray-500">
+                No offers
+            </div>
+        </Box>
+        <div v-else class="md:col-span-7 items-center">Displayed when there are offers</div>
+        <Box class="md:col-span-5">
+            <template #header>Basic Info:</template>
+            <Price :price="listing.price" class="text-2xl font-bold" />
+
+            <ListingSpace :listing="listing" class="text-lg"/>
+            <ListingAddress :listing="listing" class="text-gray-500" />
+
+        </Box>
+    </section>
 </template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3';
+import Box from '@/Components/UI/Box.vue';
+import Price from '@/Components/Price.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
+import ListingAddress from '@/Components/ListingAddress.vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+    listing: Object
+});
+
+const hasOffers = computed(() => props.listing.offers.length)
+</script>
