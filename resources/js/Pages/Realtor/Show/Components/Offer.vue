69a56354d0c913b3e2935b6a406c749c41085e61
NoOPeEKS <arnauapps@gmail.com>
2023-12-09 23:01:27 +0100

    Accepted offers show accepted badge, and no offer can be accepted anymore

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,6 +1,6 @@
 <template>
     <Box>
-        <template #header>Offer #{{ offer.id }}</template>
+        <template #header>Offer #{{ offer.id }} <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">Accepted</span></template>
         <section class="flex items-center justify-between">
             <div>
                 <Price class="text-xl" :price="offer.amount"/>
@@ -16,6 +16,7 @@
             </div>
             <div>
                 <Link 
+                    v-if="notSold"
                     :href="route('realtor.offer.accept', {offer: offer.id})" 
                     class="btn-outline text-xs font-medium" 
                     as="button"
@@ -40,4 +41,5 @@ const props = defineProps({
 
 const difference = computed(() => props.offer.amount - props.listingPrice);
 const madeOn = computed(() => new Date(props.offer.created_at).toDateString());
+const notSold = computed(() => !props.offer.accepted_at && !props.offer.rejected_at)
 </script>
