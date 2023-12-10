dffff7b76cd660ab310c59957c99c026c3467887
NoOPeEKS <arnauapps@gmail.com>
2023-12-10 15:13:03 +0100

    Created Notification for Offers Made

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Offer;
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use App\Notifications\OfferMade;
 
 class ListingOfferController extends Controller
 {
     public function store(Listing $listing, Request $request) {
         $this->authorize("view", $listing);
         
-        $listing->offers()->save(
+        $offer = $listing->offers()->save(
             Offer::make(
                 $request->validate([
                     'amount' => 'required|integer|min:1|max:20000000'
                 ])
             )->bidder()->associate($request->user())
         );
+
+        $listing->owner->notify(new OfferMade($offer));
 
         return redirect()->back()->with('success','Offer bidded successfully!');
     }
